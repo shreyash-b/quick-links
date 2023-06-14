0f2fb1d3422e35f08eaf780e700bfe387dbb870f
Shreyash Bubane <52196231+shreyash-b@users.noreply.github.com>
2023-06-15 00:36:42 +0530

    added title+username bar

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -7,10 +7,7 @@ type layoutProp = {
 export default function Layout({ children } : layoutProp) {
   return (
     <>
-      <h1 className="text-5xl text-center select-none py-5"> Quick Links </h1>
-      <hr className="mx-4" />
       { children }
-
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex w-full h-full justify-center items-center flex-col">
+      <h1 className="text-5xl text-center select-none py-5"> Quick Links </h1>
+
+      <span className="text-2xl">
+        <input
+          type="text"
+          placeholder="Enter username..."
+          className="max-w-[15rem] outline-none p-3 m-1 bg-[#ffffff0f] border-solid border-teal-800 border-2 rounded"
+        />
+        <input type="button" value="GO!" className="m-1 border-solid border-2 border-teal-800 rounded p-3" />
+      </span>
+    </div>
+  );
 }
