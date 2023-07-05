93c1595a021d9e2ac5a2aed58f32d0523536fa1f
Shreyash Bubane <52196231+shreyash-b@users.noreply.github.com>
2023-07-06 00:27:50 +0530

    improve text formatting on links screen

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import LinkCard, { CardInfo } from "../Components/LinkCard";
 import useSWR from "swr";
 import AddBtn from "../Components/AddBtn";
+import { link } from "fs";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -14,15 +15,21 @@ export default function UserPage() {
   var links: CardInfo[] = [];
 
   if (data) {
-    links = data[id];
+    const returnedLinks = data[id];
+    if (returnedLinks) {
+      links = returnedLinks;
+    }
   }
 
   return (
-    <div className="flex w-full h-full flex-col justify-center items-center gap-5">
+    <div className="flex w-full h-full flex-col justify-center items-center gap-5 text-center">
       <div className="text-4xl">Welcome back {id}!</div>{" "}
       {/*TODO: gradient on name*/}
       <div className="text-2xl text-gray-400">
-        Here are the links you saved:{" "}
+        {links.length !== 0
+          ? "Here are the links you saved:"
+          : <span>You haven't saved any link!! <br />
+                   Click here to save one:</span>}
       </div>
       <div className="flex flex-wrap gap-4 justify-center max-w-[36rem]">
         {links.map((link, index) => {
