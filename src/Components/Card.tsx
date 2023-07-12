7d82531fcf40370c345ae5f6aa6ff529a72f56f2
Shreyash Bubane <52196231+shreyash-b@users.noreply.github.com>
2023-07-13 01:39:53 +0530

    edit button pencil icon added

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,13 +1,9 @@
 import { ReactElement, ReactPropTypes } from "react";
 
-type CardProps = {
-    children: ReactElement
-}
-
-export default function Card(props: CardProps) {
+export default function Card(props: any) {
   return (
-    <div className="aspect-square w-[8rem] text-xl grid place-items-center place-content-center text-center text-white-100 font-thin bg-white/[0.15] rounded-xl">
-        {props.children}
+    <div className={`aspect-square w-[8rem] text-xl grid place-items-center place-content-center text-center text-white-100 font-thin bg-white/[0.15] rounded-xl ${props.className}`}>
+      {props.children}
     </div>
   );
 }
--- src/Components/LinkCard.tsx
+++ src/Components/LinkCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Card from "./Card";
+import Button from "./Button";
 
 type CardProps = {
   cardInfo: {
@@ -8,12 +9,27 @@ type CardProps = {
   };
 };
 
+function PencilIcon(props: any) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" {...props}>
+      <path d="M794.717-664.087 665.761-793.043l41.043-41.283q17.718-17.717 44.055-17.337 26.337.38 44.771 18.337l40.609 40.369q18.435 18.196 17.837 44.153-.598 25.956-18.315 43.674l-41.044 41.043ZM152.87-117.13q-14.674 0-24.37-9.696-9.696-9.696-9.696-24.37v-81.217q0-6.717 2.359-12.554 2.359-5.837 7.837-11.316l494.761-494.76 129.196 128.717-494.761 495q-5.718 5.478-11.555 7.837t-12.554 2.359H152.87Z" />
+    </svg>
+  );
+}
+
 export default function LinkCard(props: CardProps) {
   return (
-    <Card>
-      <Link href={props.cardInfo.linkAddress} className="h-full w-full grid place-content-center" target="_blank">
+    <Card className="relative">
+      <Link
+        href={props.cardInfo.linkAddress}
+        className="h-full w-full grid place-content-center"
+        target="_blank"
+      >
         {props.cardInfo.linkTitle}
       </Link>
+      <button className="absolute w-fit h-fit border-l-neutral-100 border-x-[1px] rounded-lg p-[1px] fill-slate-500 right-1 top-1">
+        <PencilIcon className="w-6 aspect-square" />
+      </button>
     </Card>
   );
 }
